e3432f4868aed2b0476e48f79cbe4eb4ff52aaa9
Nazar41 <lollogin13@gmail.com>
2022-01-11 17:25:30 +0200

    lesson

---
+++ rokyyfps/lesson29/script.js
@@ -0,0 +1,44 @@
+// let x = 10
+// let y = 5
+// console.log(x + y)
+
+
+
+// let as = 'helo wrld'
+// console.log(as)
+// alert(as)
+
+// let userAge = prompt('Сколько лет?')
+// let userName = prompt('Как зовут?')
+// let userCard = prompt('Номер карты?')
+// const userData = (userAge + ' ' + userName + ' ' + userCard)
+
+// console.log(userData)
+
+
+// let x = prompt('Назови первое число ')
+// let y = prompt('Назови второе число ')
+// x = Number(x)
+// y = Number(y)
+// console.log(x + y)
+
+let req = prompt('Впиши 1 либо 2!')
+const answer1 = ('Ты выбрал 1, я угадал')
+const answer2 = ('Ты выбрал 2, я угадал')
+const error = ('1000-7?')
+
+if (req == (1)){
+    alert(answer1)
+}
+
+else if (req == (2)){
+    alert(answer2)
+}
+
+else{
+    alert(error)
+}
+
+
+
+
