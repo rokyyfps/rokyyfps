7ebf27c9f1b61f90558e3d18dc124652d86638eb
Nazar41 <lollogin13@gmail.com>
2022-01-17 18:01:26 +0200

    lesson

---
+++ rokyyfps/lesson30/js/script.js
@@ -0,0 +1,52 @@
+// let q = 'srt'
+// let w = 1
+// let e = true
+// let r = (1/0)
+// let t = Number('qq')
+// let y = null
+
+
+// console.log(typeof(q));
+// console.log(typeof(w));
+// console.log(typeof(e));
+// console.log(typeof(r));
+// console.log(t);
+// console.log(typeof(y));
+// console.log(typeof(a));
+
+// let a = 2
+// let x = 10
+// let z = 9
+
+// let y = 7*Math.pow(x, 6) + Math.sqrt(a) + Math.pow(x, 3) - (z/9) + Math.pow(a, x)
+// console.log(y);
+
+
+// Задание 1
+// let y
+// let x = prompt('Задай x')
+//     // if (x > 0){
+//     //     y = 2*Math.sin(x)
+//     // } else{
+//     //     y = (6 - x)
+//     // }
+//     // alert(y)
+
+//     // Задание 2
+//     // if (x < -2 || x > 2){
+//     //     y = 2*x
+//     // } else{
+//     //     y = -3*x
+//     // }
+//     // alert(y)
+
+//     // Задание 3
+//     if (x <= 0){
+//         y = -x
+//     } else if (x > 0 & x < 2){
+//         y = Math.pow(x, 2)
+//     } else{
+//         y = 4
+//     }
+    
+//     alert(y)
---
+++ rokyyfps/lesson31/js/script.js
@@ -0,0 +1,56 @@
+// let x = prompt('введи число')
+// x = Number(x)
+
+// // задание 1-2
+// if (x > 0) {
+//     x += 1
+//     console.log(x);
+// } else {
+//     x -= 2
+//     console.log(x);
+// }
+
+
+// Задание 4
+
+let x = prompt('Введите число 1')
+let y = prompt('Введите число 2')
+let z = prompt('Введите число 3')
+let c = 0
+let g = 0
+// let massive = []
+// if (!isNaN(x) & x > 0) {
+//     massive.push(x)
+// }
+// if (!isNaN(y) & y > 0 ) {
+//     massive.push(y)
+// }
+// if (!isNaN(z) & z > 0) {
+//     massive.push(z)
+// } 
+
+
+if (x > 0) {
+    c++
+}
+if (y > 0) {
+    c++
+}
+if (z > 0) {
+    c++
+}
+
+if (x < 0) {
+    g++
+}
+if (y < 0) {
+    g++
+}
+if (z < 0) {
+    g++
+}
+
+console.log(g);
+console.log(c);
+// console.log(massive.length)
+// console.log(massive)
