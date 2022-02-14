b268b742c1f4f4e13a789a48bef7c4169e7dc9a7
fokyyfps <filimonovs142@gmail.com>
2022-02-14 17:49:33 +0200

    js

--- rokyyfps/lesson34/js/script.js
+++ rokyyfps/lesson34/js/script.js
@@ -49,27 +49,69 @@ function boolean34(){
 function if29(){
     let a = prompt('Введи число')
     let massive = []
-    if(Math.sqrt(a**2) = a | a != 0){
-        massive.push('Положительное')
-    } else if(Math.sqrt(a**2) != a | a != 0){
-        massive.push('Отрицательное')
+    let res = ''
+    if(!isNaN(a)){
+        if(a > 0){
+            massive.push('Положительное')
+        } else if(a < 0){
+            massive.push('Отрицательное')
+        }
+        if(a%2 == 0){
+            massive.push('чётное')
+        } else if(a%2 != 0){
+            massive.push('не чётное')
+        }
+        if(a == 0){
+            massive.push('это ноль')
+        } 
     }
-    if((a%2) = 0){
-        massive.push('чёьное')
-    } else if((a%2) != 0){
-        massive.push('чётное')
-    }
-    if(a = 0){
-        massive.push('это ноль')
-    } else{
+    if(isNaN(a)){
         massive.push('это не число')
     }
+    for(let i = 0; i < massive.length; i++){
+        res = res + ' ' + massive[i] 
+    }
+    console.log(res + ' число');
+}
+
+let if30 = () => {
+    let a = prompt('Введи число')
+    let massive = []
+    let res = ''
+    if(a % 2 == 0){
+        massive.push('чётное')
+    } else if(a % 2 != 0){
+        massive.push('не чётное')
+    }
+    if(a / 100 >= 1){
+        massive.push('трёхзначное')
+    } else if(a / 10 >= 1){
+        massive.push('двухзначное')
+    } else{
+        massive.push('это цифра')
+    }
+    for(let i = 0; i < massive.length; i++){
+        res = res + ' ' + massive[i] 
+    }
+    console.log(res + ' число');
 }
 
 
-
-
-
+let for3 = () => {
+    let a = 0
+    let b = 0
+    massive = []
+    while(a >= b || a == '' || b == ''){
+        a = prompt('Задай а')
+        b = prompt('Задай b, которое больше a')
+    }
+    for(let i = 0; a < b; i++){
+        massive.push(a++)
+    }
+    massive.push(b)
+    massive.unshift()
+    console.log(massive);
+}
 
 
 
